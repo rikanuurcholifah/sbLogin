fcdd0a9997a99fa19c64f262317497b97a1c4719
unknown <rikanuurcholifah@gmail.com>
2021-02-04 16:28:53 +0700

    Menu Management 2

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -45,30 +45,34 @@
         $subMenu = $this->db->query($qureyMenu)->result_array();
         ?>
         <?php foreach ($subMenu as $sm) : ?>
-            <!-- Nav Item - Dashboard -->
-            <li class="nav-item">
+            <?php if ($title == $sm['title']) : ?>
+                <!-- Nav Item - Dashboard -->
+                <li class="nav-item active">
+                <?php else :  ?>
+                <li class="nav-item">
+                <?php endif; ?>
                 <a class="nav-link" href="<?= base_url($sm['url']); ?>">
                     <i class="<?= $sm['icon']; ?>"></i>
                     <span><?= $sm['title']; ?></span></a>
-            </li>
+                </li>
+            <?php endforeach; ?>
+            <!-- Divider -->
+            <hr class="sidebar-divider">
+
         <?php endforeach; ?>
+
+        <li class="nav-item">
+            <a class="nav-link" href="<?= base_url('auth/logout'); ?>">
+                <i class="fas fa-fw fa-sign-out-alt"></i>
+                <span>Logout</span></a>
+        </li>
         <!-- Divider -->
-        <hr class="sidebar-divider">
+        <hr class="sidebar-divider d-none d-md-block">
 
-    <?php endforeach; ?>
-
-    <li class="nav-item">
-        <a class="nav-link" href="<?= base_url('auth/logout'); ?>">
-            <i class="fas fa-fw fa-sign-out-alt"></i>
-            <span>Logout</span></a>
-    </li>
-    <!-- Divider -->
-    <hr class="sidebar-divider d-none d-md-block">
-
-    <!-- Sidebar Toggler (Sidebar) -->
-    <div class="text-center d-none d-md-inline">
-        <button class="rounded-circle border-0" id="sidebarToggle"></button>
-    </div>
+        <!-- Sidebar Toggler (Sidebar) -->
+        <div class="text-center d-none d-md-inline">
+            <button class="rounded-circle border-0" id="sidebarToggle"></button>
+        </div>
 
 </ul>
 <!-- End of Sidebar -->
